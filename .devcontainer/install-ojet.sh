3d77767de6bf552070aa57dd8cc2df648915b834
Jose Fajardo <fajardo_jf@hotmail.com>
2020-03-14 20:13:12 +1100

    + ojet-cli

--- .devcontainer/install-ojet.sh
+++ .devcontainer/install-ojet.sh
@@ -1,2 +1,5 @@
-git clone https://github.com/oracle/oraclejet.git /tmp/gh/oracle
+git clone https://github.com/oracle/oraclejet.git /tmp/gh/oracle/oraclejet
+git clone https://github.com/oracle/ojet-cli.git /tmp/gh/oracle/ojet-cli
 # mv /tmp/gh /workspaces/ojet-gui
+npm install -g @oracle/ojet-cli
+
